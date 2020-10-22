6d390d7d7d57deab874247995cca74b038d49b65
Santosh Narwade <santosh.narawade1@gmail.com>
2020-10-22 17:15:40 +0530

    adding nodejs server

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-var http = require("http");
+// var http = require("http"),
+fs = require("fs");
+// const express = require("express");
+// const app = express();
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
-  })
-  .listen(process.env.PORT || 3000, () => {
-    console.log("Server running on 3000");
+// const bodyParser = require("body-parser");
+
+// app.use(bodyParser.json());
+// app.get("/", function (req, res) {
+//   console.log("Here ?@@@@@@@@@@@@@@@@@@@@@@@@ ");
+// });
+// app.use(express.static(process.cwd() + "/"));
+
+// http
+//   .createServer(function (req, res) {
+//     console.log("Started");
+//     res.writeHead(200);
+//     res.end("Welcome to Node.js HTTPS Servern");
+//   })
+//   .listen(process.env.PORT || 3000, () => {
+//     console.log("Server running on 3000");
+//   });
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/index.html");
+});
+app.get("*", (req, res) => {
+  const extention = req.url.split(".")[req.url.split(".").length - 1];
+  if (extention !== "html") {
+    res.sendFile(path.join(__dirname, req.url));
+  } else {
+    res.sendFile(process.cwd() + "/index.html");
+  }
+});
+
+function onRequest(request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./index.html", null, function (error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write("File not found");
+    } else {
+      response.write(data);
+    }
+    response.end();
   });
+}
+// app.use(function (req, res) {
+//   res.sendFile(process.cwd() + "/");
+//   //   console.log("404 ", req);
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
